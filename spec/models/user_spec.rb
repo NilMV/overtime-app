5b17072b029715a113c80843010e07d9e727e613
NilMV <nil.medvedev17@gmail.com>
2017-12-02 16:50:04 +0300

    + full name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "creation" do
-    before do
-      @user = User.create(email: "test@test.com", 
-                          password: "qwerty", 
-                          password_confirmation: "qwerty", 
-                          first_name: "Jaja", 
-                          last_name: "lol")
-    end
+  before do
+    @user = User.create(email: "test@test.com", 
+                        password: "qwerty", 
+                        password_confirmation: "qwerty", 
+                        first_name: "Jon", 
+                        last_name: "Snow")
+  end
 
+  describe "creation" do
     it "can be created" do
       expect(@user).to be_valid
     end
@@ -18,6 +18,12 @@ RSpec.describe User, type: :model do
       @user.first_name = nil
       @user.last_name = nil
       expect(@user).to_not be_valid
+    end
+  end
+
+  describe "custom name methods" do
+    it 'has a full name method: fn +ln' do
+      expect(@user.full_name).to eq("SNOW, JON")
     end
   end
 end
